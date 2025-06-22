9643e4956fd6c467025249a823b9030519cff2de
ethanc8 <echaroenpitaks@gmail.com>
2025-06-22 15:22:27 -0500

    Fix ExtraDefines.h to reflect fixes in gnustep-gui

--- ExtraDefines.h
+++ ExtraDefines.h
@@ -6,15 +6,6 @@ typedef uint8_t UInt8;
 typedef uint32_t UInt32;
 
 #ifdef GNUSTEP
-enum {
-  NSColorSpaceModelUnknown = NSUnknownColorSpaceModel,
-  NSColorSpaceModelGray = NSGrayColorSpaceModel,
-  NSColorSpaceModelRGB = NSRGBColorSpaceModel,
-  NSColorSpaceModelCMYK = NSCMYKColorSpaceModel,
-  NSColorSpaceModelLAB = NSLABColorSpaceModel,
-  NSColorSpaceModelDeviceN = NSDeviceNColorSpaceModel
-};
-
 typedef enum _NSUnderlineStyle NSUnderlineStyle;
 
 typedef NSString * NSKeyValueChangeKey;
